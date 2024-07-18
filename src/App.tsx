af03e02da0793e7f702e90c4b552f05fbde0cabe
Samonnite <981003163@qq.com>
2024-07-18 16:45:13 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import twaLogo from './assets/tapps.png'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import twaLogo from "./assets/tapps.png";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-import WebApp from '@twa-dev/sdk'
+import WebApp from "@twa-dev/sdk";
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Tabs position="relative" variant="unstyled">
+        <TabList>
+          <Tab
+            fontSize="sm"
+            lineHeight="18px"
+            color="#777885"
+            fontWeight="bold"
+            _selected={{ color: "white" }}
+            px="0"
+            mr="3"
+          >
+            进行中
+          </Tab>
+          <Tab
+            fontSize="sm"
+            lineHeight="18px"
+            color="#777885"
+            fontWeight="bold"
+            _selected={{ color: "white" }}
+            px="0"
+          >
+            已结束
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel px="0" pt="3">
+            1
+          </TabPanel>
+          <TabPanel px="0" pt="3">
+            2
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
       <div>
         <a href="https://ton.org/dev" target="_blank">
           <img src={twaLogo} className="logo" alt="TWA logo" />
@@ -30,12 +64,16 @@ function App() {
       </div>
       {/*  */}
       <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
+        <button
+          onClick={() =>
+            WebApp.showAlert(`Hello World! Current count is ${count}`)
+          }
+        >
+          Show Alert
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
